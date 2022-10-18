4fb8dc080046d62d5c646f340ae630b3d9d91d2e
Pavel <dabdab@mail.ru>
2022-10-18 18:25:11 +0300

    таблица кубов от 1 до N

---
+++ Test3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число - ");
+int n = int.Parse(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < n; i++)
+    {
+    array[i] = Math.Pow(i+1, 3) ;
+    {
+        Console.Write($" {array[i] }");
+        }
+    }
